bb292a3bc02faa8dcd7fcee0afb4cef9cad60c1a
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-09-17 02:37:31 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 "use client";
-import Navbar from "@/components/Narbar";
+import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import { use } from "react";
