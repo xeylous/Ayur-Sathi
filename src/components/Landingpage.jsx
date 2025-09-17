211b75a6a2b877caaa53437372c9e292d49b513f
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-09-17 16:42:02 +0530

    Update Landingpage.jsx

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -3,14 +3,14 @@
 import React, { Suspense } from "react";
 import Home_1 from "@/components/Home_1";
 import Home_2 from "@/components/Home_2";
-import IndexContent from "@/components/Index"; 
+import Index from "@/components/Index"; 
 import LandingSkeleton from "@/components/LandingSkeleton";
 
 function Landingpage() {
   return (
     <Suspense fallback={<LandingSkeleton />}>
       <div>
-        <IndexContent />
+        <Index />
         <Home_1 />
         <Home_2 />
       </div>
