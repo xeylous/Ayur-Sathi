c72da6af40c4756e476675718bbd28b1855f9c9a
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-09-21 16:32:52 +0530

    redis added

--- src/models/Farmer.js
+++ src/models/Farmer.js
@@ -6,6 +6,9 @@ const FarmerSchema = new mongoose.Schema(
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true }, // unique within farmers only
     password: { type: String, required: true },
+    uniqueId: { type: String, required: true, unique: true },
+    type: { type: String, required: true },
+    verified: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
