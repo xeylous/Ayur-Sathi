7c28ced609bf84ba8cd5193621585ead52af8b7f
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2025-11-22 11:59:25 +0530

    Update page.js

--- apps/web/src/app/herbslib/page.js
+++ apps/web/src/app/herbslib/page.js
@@ -2,7 +2,7 @@
 
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Cropinfo from "@/components/cropinfo";
+import Cropinfo from "@/components/Cropinfo";
 
 
 export default function Home() {
