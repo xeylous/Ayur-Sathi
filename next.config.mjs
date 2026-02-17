25f1b2db18930ad2b2064322f1bed4c327ce5b13
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2026-02-17 10:30:31 +0530

    Enhanced recharts config for Vercel compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     transpilePackages: ['recharts'],
+    experimental: {
+        serverComponentsExternalPackages: ['recharts'],
+    },
+    webpack: (config) => {
+        config.externals = [...(config.externals || []), { canvas: 'canvas' }];
+        return config;
+    },
 };
 
 export default nextConfig;
