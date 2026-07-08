feb75025971855a6f8f9904c8b8e909660830b49
APURV SINHA <119706011+xeylous@users.noreply.github.com>
2026-07-08 15:32:10 +0530

    Update redis.js

--- src/lib/redis.js
+++ src/lib/redis.js
@@ -6,7 +6,7 @@ if (!global.redis) {
   redis = new Redis({
     host: process.env.REDIS_HOST || "127.0.0.1",
     port: process.env.REDIS_PORT || 6379,
-    // password: process.env.REDIS_PASSWORD || undefined,
+    password: process.env.REDIS_PASSWORD || undefined,
   });
 
   // Optional: for dev hot reload to not create multiple connections
