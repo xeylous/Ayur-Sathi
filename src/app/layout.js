a75563b380e91867679c846cbeb58f6d96ff0176
xeylous <apurvsinha2003@gmail.com>
2026-07-23 20:03:51 +0530

    feature: chatbot added

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ import { FarmerProvider } from "@/context/FarmerContext";
 import { CropProvider } from "@/context/CropContext"; 
 import { Analytics } from '@vercel/analytics/next';
 import SocketInitializer from "./SocketInitializer";
+import ChatbotAssistant from "@/components/ChatbotAssistant";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -48,6 +49,7 @@ export default function RootLayout({ children }) {
         <Analytics />
         </CropProvider>
         </FarmerProvider>
+        <ChatbotAssistant />
         </AuthProvider>
       </body>
     </html>
